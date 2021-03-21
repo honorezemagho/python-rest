fba2c49dde7ae78a073337d458d9031090ecd091
honorezemagho <honorezemagho@gmail.com>
2021-03-22 00:52:58 +0100

    restart all from scratch due to an error

--- core/models.py
+++ core/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Document(models.Model): 
-    PP = 'PP'
-    ID = 'ID'
-    OT = 'OT'
-    DOC_TYPES = [
-        (PP, 'Passport'),
-        (ID, 'Identity Card'),
-        (OT, 'Others')
-    ]
-    dtype = models.CharField(choices=DOC_TYPES, max_length=2)
-    doc_number = models.CharField(max_length=50)
-    
-    def __str(self): 
-        return self.doc_number
-
-class Customer(models.Model): 
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    document = models.ManyToManyField(Document)
-
-
-    def __str(self): 
-        return self.name
--- customers/asgi.py
+++ customers/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for customer_base project.
+ASGI config for customers project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customer_base.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customers.settings')
 
 application = get_asgi_application()
