3d789de4df74bfb11a5eaa538780caae8d75209b
honorezemagho <honorezemagho@gmail.com>
2021-03-22 01:07:23 +0100

    add models and register core into installed apps

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Profession(models.Model): 
+    description = models.CharField(max_length=50)
+class DataSheet(models.Model): 
+    description = models.CharField(max_length=50)
+    historical_data = models.CharField(max_length=50)
+class Customer(models.Model): 
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    profession = models.ManyToManyField(Profession)
+    data_sheet = models.OneToOneField(DataSheet, on_delete=models.CASCADE,null=True)
+
+    def __str(self): 
+        return self.name
+
+class Document(models.Model): 
+    PP = 'PP'
+    ID = 'ID'
+    OT = 'OT'
+    DOC_TYPES = [
+        (PP, 'Passport'),
+        (ID, 'Identity Card'),
+        (OT, 'Others')
+    ]
+    dtype = models.CharField(choices=DOC_TYPES, max_length=2)
+    doc_number = models.CharField(max_length=50)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE,null=True)
+    
+    def __str(self): 
+        return self.doc_number
